086cf5f443766e8a4384f9f4d6db4fd495c5309e
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2024-05-02 19:15:23 -0400

    fonction relaxation

--- question_2a.py
+++ question_2a.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def relaxation(v, r, z,):
+    if r > 0:
+        return V
+    
+if __name__ == '__main__':
+    pass
--- question_3b.py
+++ question_3b.py
@@ -1,2 +1,2 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
