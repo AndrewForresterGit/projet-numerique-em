3f77d7099fd3a1b9b1454211c580eb6a1dcfa940
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2024-05-02 19:16:11 -0400

    fonction relaxation

--- question_2a.py
+++ question_2a.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def relaxation(v, r, z,):
+def relaxation(v, r, z, h):
     if r > 0:
-        return V
+        return (v[r+1,z]+v[r-1,z]+v[r,z+1]+v[r,z-1])/4 + h*(v[r+1,z]-v[r-1,h])/(8*r)
+    else:
+        pass
     
 if __name__ == '__main__':
     pass
