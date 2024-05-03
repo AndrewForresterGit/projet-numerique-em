75515d194c061e16f0ebe9a57f9996bdf77427ec
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2024-05-02 20:01:29 -0400

    completion fonction relaxation, essay avec meshgrid V

--- question_2a.py
+++ question_2a.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 def relaxation(v, r, z, h):
     if r > 0:
-        return (v[r+1,z]+v[r-1,z]+v[r,z+1]+v[r,z-1])/4 + h*(v[r+1,z]-v[r-1,h])/(8*r)
+        v[r,h] = (v[r+1,z]+v[r-1,z]+v[r,z+1]+v[r,z-1])/4 + h*(v[r+1,z]-v[r-1,h])/(8*r)
     else:
-        pass
+        v[r,h] = (4*v[h,z]+v[r,h+1]+v[r,z-1])
     
 if __name__ == '__main__':
-    pass
+    Vx, Vy = np.meshgrid(np.arange(1,10,0.1),np.arange(1,10,0.1))
+    print(Vx, Vy)
