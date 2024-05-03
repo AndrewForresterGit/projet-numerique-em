ce640d54f1489216028a12c336a8b5068c44f765
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2024-05-03 15:03:39 -0400

    graph fonctionne?

--- question_2a.py
+++ question_2a.py
@@ -11,27 +11,30 @@ def filtre(v, r, z, h, A):
 
 
 if __name__ == '__main__':
-    V = np.full((30,120), .5, dtype=float)
-    M = np.full((30,120), True, dtype=bool)
+    V = np.full((31,121), .5, dtype=float)
+    M = np.full((31,121), True, dtype=bool)
 
     M[:,:] = True
     V[:,0], M[:,0] = 0, False
     V[0,:75], M[0,:75] = 0, False
     V[-1,:], M[-1,:] = -300, False
 
-    R, Z = np.arange(0, 30, 1), np.arange(0, 120, 1)
+    R, Z = np.arange(0, 31, 1), np.arange(0, 121, 1)
     R1, Z1 = np.meshgrid(Z, R)
     V, M = np.where(R1 + Z1 > 118, -300, V), np.where(R1 + Z1 > 118, False, M)
     
-    fig = plt.figure(figsize=(12, 3),layout='constrained')
+    fig = plt.figure(figsize=(12, 6),layout='constrained')
     ax = fig.subplots()
 
-    for n in range(0, 1000):
+    for n in range(0, 2500):
         for i in range(0, len(V)):
             for j in range(0, len(V[i])):
                 filtre(V, i, j, 0.1, M[i,j])
 
     V = np.where(R1 + Z1 > 119, np.NaN, V)
 
-    ax.contourf(R1, Z1, V, levels=100, cmap='plasma')
+    ax.set_xlim(0,120)
+    ax.set_ylim(0, 60)
+    ax.matshow(np.concatenate((np.flip(V[1:,:], axis=0), V), axis=0), cmap='inferno')
+    # ax.contourf(np.concatenate((np.flip(V[1:,:], axis=0), V), axis=0), levels=100, cmap='inferno')
     plt.show()
